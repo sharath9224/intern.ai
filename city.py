c0bb9839c9b65fc824dc440721a89a86fbe9e8e7
sharath9224 <110328656+sharath9224@users.noreply.github.com>
2023-05-22 15:29:26 +0530

    first commit

---
+++ city.py
@@ -0,0 +1,11 @@
+
+ #1. Create a python code using Pandas to filter a dataset CSV file and group the results to find the most cities with Male population.
+import pandas as pd
+data ={"Name":['New York','New York','Los Angeles','Los Angeles','Chicago','Chicago','Houston','Houston','Phoenix','Phoenix','Philadelphia','Philadelphia'],
+"gender":['male','female','male','female','male','female','male','female','male','female','male','female'],
+"population":['4125000','4300000','1985000','2070000','1350000','1400000','1100000','1120000','800000','810000','750000','770000']}
+df=pd.DataFrame(data)
+print(df)
+#Filtered_df=df[df['Name','gender']]
+gender.sort()
+print(gender)
---
+++ vvvvv.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app =Flask(_name_)
+@app.route('/api/data',method=['get'])
+def get_data():
+data ={"Name":['New York','New York','Los Angeles','Los Angeles','Chicago','Chicago','Houston','Houston','Phoenix','Phoenix','Philadelphia','Philadelphia'],
+"gender":['male','female','male','female','male','female','male','female','male','female','male','female'],
+"population":['4125000','4300000','1985000','2070000','1350000','1400000','1100000','1120000','800000','810000','750000','770000']}
+return jsonify()
+if name ='main'
+app run()
